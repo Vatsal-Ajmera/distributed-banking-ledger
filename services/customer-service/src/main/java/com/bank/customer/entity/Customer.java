01028e05b77dadef13e03acf08b330e80a052c3a
Vatsal Ajmera <vatsal.ajmera69@gmail.com>
2026-03-19 19:19:41 +0530

    Added basic validation to Customer entity and controller

--- services/customer-service/src/main/java/com/bank/customer/entity/Customer.java
+++ services/customer-service/src/main/java/com/bank/customer/entity/Customer.java
@@ -1,6 +1,8 @@
 package com.bank.customer.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,12 +26,16 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String name;
 
+    @Email
+    @NotBlank
     @Column(nullable = false, unique = true)
     private String email;
 
+    @NotBlank
     @Column(nullable = false)
     private String passwordHash;
 
